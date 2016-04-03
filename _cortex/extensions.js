626913f21335b68517521fcbeec554f5ed4d5f37
RodrigoBuas <rodrigobuas@gmail.com>
2016-04-04 01:48:56 +0200

    shared cortex

--- _cortex/extensions.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-
-    Array.prototype.unique = function(a){
-        return function(){ return this.filter(a) }
-    }(function(a,b,c){ return c.indexOf(a,b+1) < 0 });
-
-})(jQuery);
