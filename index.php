6a68fe7fd8dce13c2fd78e237512b563cab85132
RodrigoBuas <rodrigobuas@gmail.com>
2016-05-31 08:47:58 +0200

    master user

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ ini_set('display_errors', '1');
 error_reporting( E_ALL );
 set_time_limit(30);
 set_include_path('.');
-define("MASTER", "rodrigobuas@gmail.com");
 define("POSTMAN", "rodrigobuas@gmail.com");
 define("POSTNAME", "Rodrigo Buas");
 define("POSTTAG", "RBUAS");
